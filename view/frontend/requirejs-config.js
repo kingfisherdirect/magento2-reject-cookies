0e7056055052b1f39271dc2df6866480690baa1d
Kamil <lumnn@mailfence.com>
2023-06-02 13:42:32 +0200

    Correct consent mode for Gtag set-up

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
     map: {
         '*': {
-            'Magento_GoogleAnalytics/js/google-analytics': 'KingfisherDirect_RejectCookies/js/google-analytics'
+            'Magento_GoogleAnalytics/js/google-analytics': 'KingfisherDirect_RejectCookies/js/google-analytics',
+            'Magento_GoogleGtag/js/google-analytics': 'KingfisherDirect_RejectCookies/js/gtag-google-analytics'
         }
     },
 };
