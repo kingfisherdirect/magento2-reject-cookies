72973d90f0f3934b95fd06da4e9178ad69b19c5f
Kamil <lumnn@mailfence.com>
2023-07-12 16:48:13 +0200

    Disable consent

--- view/frontend/web/js/gtag-google-analytics.js
+++ view/frontend/web/js/gtag-google-analytics.js
@@ -14,39 +14,8 @@ define([
      * @param {Object} config
      */
     return function (config) {
-        var allowServices = false,
-            allowedCookies,
-            allowedWebsites,
-            measurementId;
-
-        if (config.isCookieRestrictionModeEnabled) {
-            allowedCookies = $.mage.cookies.get(config.cookieName);
-
-            if (allowedCookies !== null) {
-                allowedWebsites = JSON.parse(allowedCookies);
-
-                if (allowedWebsites[config.currentWebsite] === 1) {
-                    allowServices = true;
-                }
-            }
-        } else {
-            allowServices = true;
-        }
-
-        function setupConsent () {
-            if (allowServices) {
-                return
-            }
-
-            gtag('consent', 'default', {
-              'ad_storage': 'denied',
-            });
-
-            $(document).on('user:allowed:save:cookie', grantConsent);
-        }
-
         /* Global site tag (gtag.js) - Google Analytics */
-        measurementId = config.pageTrackingData.measurementId;
+        var measurementId = config.pageTrackingData.measurementId;
 
         if (!window.gtag) {
             (function(d,s,u){
@@ -58,14 +27,12 @@ define([
             })(document, 'script', 'https://www.googletagmanager.com/gtag/js?id=' + measurementId);
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
-            setupConsent()
             gtag('js', new Date());
             gtag('set', 'developer_id.dYjhlMD', true);
-        } else {
-            setupConsent()
         }
 
         gtag('config', measurementId, { 'anonymize_ip': true });
+
         // Purchase Event
         if (config.ordersTrackingData.hasOwnProperty('currency')) {
             var purchaseObject = config.ordersTrackingData.orders[0];
@@ -73,13 +40,5 @@ define([
             purchaseObject['currency'] = config.ordersTrackingData.currency;
             gtag('event', 'purchase', purchaseObject);
         }
-    }
-
-    function grantConsent () {
-        gtag('consent', 'default', {
-          'ad_storage': 'granted'
-        });
-
-        $(document).off('user:allowed:save:cookie', grantConsent);
     }
 });
